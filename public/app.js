39ae000e2c91d3fae55f169322a3657245c90425
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-09 22:24:35 +0100

    refactors adding cats section by javascript only rather than html

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const createCat = function() {
   let cat = document.createElement("ul");
+  cat.classList.add("cat");
   return cat;
 }
 
@@ -24,16 +25,33 @@ const image = function(path) {
   return imgLi;
 }
 
-const addCat = function(name, food, path) {
+const addCat = function(catToAdd) {
   let cat = createCat();
-  cat.appendChild(nameCat(name));
-  cat.appendChild(faveFood(food));
-  cat.appendChild(image(path));
-  let cats = document.querySelector("#cats");
-  cats.appendChild(cat);
+  cat.appendChild(nameCat(catToAdd.name));
+  cat.appendChild(faveFood(catToAdd.food));
+  cat.appendChild(image(catToAdd.path));
+  return cat;
 }
 
+var Cat = function(name, food, path) {
+  this.name = name;
+  this.food = food;
+  this.path = path;
+}
+
+const cat1 = new Cat("Boba", "sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+const cat2 = new Cat("Barnaby", "tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+const cat3 = new Cat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
+const cat4 = new Cat("Chicken", "pizza", "cat_photo.jpeg");
+let catsToAdd = [cat1, cat2, cat3, cat4];
+
 const app = function() {
-  addCat("Chicken", "Pizza", "cat_photo.jpeg");
+  let body = document.querySelector("body");
+  let catsSection = document.createElement("section");
+  catsSection.id = "cats";
+  body.appendChild(catsSection);
+  for(let cat of catsToAdd) {
+    catsSection.appendChild(addCat(cat));
+  }
 }
 window.onload = app;
